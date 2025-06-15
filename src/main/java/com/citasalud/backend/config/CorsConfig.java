812589f7277ae7990696c28a2eea57c4f576ac70
Johana Sevillano <63021078+johanasev@users.noreply.github.com>
2025-06-15 11:40:01 -0500

    comentado cors

--- src/main/java/com/citasalud/backend/config/CorsConfig.java
+++ src/main/java/com/citasalud/backend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.citasalud.backend.config;
+/*package com.citasalud.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,3 +21,4 @@ public class CorsConfig {
         };
     }
 }
+*/
